4951ab35375adcf84cdef043d1366761810e74a5
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-02-04 21:47:49 +0000

    Update soundgleeroombg.js

--- soundgleeroombg.js
+++ soundgleeroombg.js
@@ -24,9 +24,9 @@ function addGlobalStyle(css){
 //
 //addGlobalStyle('#button-lobby {background-image: max-height:0px;max-width:0px;}');
 //
-//addGlobalStyle('#button-vote-positive {background-image: url("http://cloud.github.com/downloads/Punkred/DnBplug/ButtonVotePositive.png") ;}');
+addGlobalStyle('#button-vote-positive {background-image: url("http://cloud.github.com/downloads/Punkred/DnBplug/ButtonVotePositive.png") ;}');
 //
-//addGlobalStyle('#button-vote-negative {background-image: url("http://cloud.github.com/downloads/Punkred/DnBplug/ButtonVoteNegative.png")!important ;}');
+addGlobalStyle('#button-vote-negative {background-image: url("http://cloud.github.com/downloads/Punkred/DnBplug/ButtonVoteNegative.png")!important ;}');
 //
 //addGlobalStyle('.chat-bouncer{background: url("https://dl.dropbox.com/u/67634625/chat-bouncer-icon.png") no-repeat 0 5px;padding-left: 17px;width: 292px;');
 //
@@ -38,7 +38,7 @@ addGlobalStyle('#room-wheel {background-image: max-height:0px;max-width:0px;}');
 //
 //addGlobalStyle('#user-fans {background-image: url("");maxheight:25px;max-width:25px;}');
 //
-addGlobalStyle('html{background: url("http://i.imgur.com/XrrgeJW.png") no-repeat scroll center top #000000;');
+addGlobalStyle('html{background: url("http://i.imgur.com/fziQ1KI.jpg") no-repeat scroll center top #000000;');
 //
 //addGlobalStyle('#button-dj-play.button-dj {background-image: url("")!important;}');
 //
